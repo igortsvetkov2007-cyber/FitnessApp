b99d2a7c787b2deb028e886c3e966b78d41f9662
igortsvetkov2007-cyber <igortsvetkov2007@gmail.com>
2026-06-30 23:40:34 +0300

    Delete TRT.py

--- TRT.py
+++
@@ -1,8 +0,0 @@
-import os
-
-path='main/TRT_main.py'
-
-if os.path.exists('main/TRT_main.py'):
-    os.system(f'python {path}')
-else:
-    print(f'Target main bot file on path {path} does not exist!')
